ac521ac506724ff31936caef0c87e50afa9bb39a
Diksha Damahe <dikshadamahe25@gmail.com>
2026-04-20 16:26:11 +0530

    fix(player): add missing flutter foundation import

--- flutter_app/lib/features/player/player_screen.dart
+++ flutter_app/lib/features/player/player_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
